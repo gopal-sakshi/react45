a57894c0cbf6030fbe759c71517af2d4aeded71c
gopal612 <gopal612@gmail.com>
2024-12-02 14:38:04 +0530

    debug vite

--- vite_project34/src/App.tsx
+++ vite_project34/src/App.tsx
@@ -8,6 +8,7 @@ function App():JSX.Element {
         <>
             <h3>welcome to react, vite, typescript23</h3>
             <h3> sum of 3, 55 == {add23(3,55)} </h3>
+            <h4> concat 23 == { add23('rma', 'cf') }  </h4>
         </>
     )
 }
--- vite_project34/src/lib23/z_maths23.ts
+++ vite_project34/src/lib23/z_maths23.ts
@@ -1,6 +1,9 @@
 export function add23 (a:number|string,b:number|string):number|string {
     if(typeof a == 'number' && typeof b == 'number') { return a+b; }
-    else return 'string_add_not_supported';
+    else {
+        console.log("ts (or) js file edi print ayindi - bundled app kada")
+        return 'string_add_not_supported';
+    }
 }
 
 export const mult23 = (a:number, b:number):number => a * b;
