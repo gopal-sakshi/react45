d441d6e931eb0e376c7cadc964262779504d6f44
gopal612 <gopal612@gmail.com>
2024-12-04 06:44:09 +0530

    esbuild options for vite-33 in ubuntu

--- vite_project33/vite.config.js
+++ vite_project33/vite.config.js
@@ -10,5 +10,12 @@ export default defineConfig({
     },
     build: {
         outDir: 'vite_build23',
-    }
+    },
+    // surprisingly ee esbuild options lekundaa ne, windows lo work ayindi... ubuntu lo enduko mari ??
+    esbuild: { loader: 'jsx', include: /.*\.jsx?$/, exclude: [] },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: { '.js': 'jsx', },
+        },
+    },
 })
